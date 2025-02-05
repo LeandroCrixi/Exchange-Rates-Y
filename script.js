edf130dfc6feee5b834c768b0a285e6bd9c89317
Leandro Crixi <leandro.crixi@hotmail.com>
2025-02-05 20:11:26 -0300

    20250205

---
+++ script.js
@@ -0,0 +1,59 @@
+async function getData() {
+    const url = "https://api.hgbrasil.com/finance?format=json-cors&key=2bc2a6be";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+    
+        const json = await response.json();
+        const usdValue = json.results.currencies.USD.buy
+        const eurValue = json.results.currencies.EUR.buy
+
+        // function to set the correct currency value
+        const brlCurrency = (locale, curr, value)=>{
+            const result = new Intl.NumberFormat(locale, { style: 'currency', currency: curr }).format(value)
+            return result
+        }
+
+        //Setting the currency name and value
+        document.querySelector('.usd-brl .initial').innerHTML = brlCurrency('pt-BR', 'BRL', usdValue)
+        document.querySelector('.brl-usd .initial').innerHTML = brlCurrency('pt-BR', 'BRL', 1/usdValue)
+        document.querySelector('.eur-brl .initial').innerHTML = brlCurrency('pt-BR', 'BRL', eurValue)
+        document.querySelector('.brl-eur .initial').innerHTML = brlCurrency('pt-BR', 'BRL', 1/eurValue)
+
+        //Setting the result of the conversion
+        //.usd-brl
+        document.querySelector('.result').innerHTML = brlCurrency('pt-BR', 'BRL', 1*usdValue)
+        document.querySelector('.usd-brl input').addEventListener('input', function(event) {
+            let value = event.target.value
+            let usdResult = document.querySelector('.usd-brl .result')
+            usdResult.innerHTML = brlCurrency('pt-BR', 'BRL', value * usdValue)
+        })
+        //.brl-usd
+        document.querySelector('.brl-usd .result').innerHTML = brlCurrency('en-US', 'USD', 1/usdValue)
+        document.querySelector('.brl-usd input').addEventListener('input', function(event) {
+            let value = event.target.value
+            let usdResult = document.querySelector('.brl-usd .result')
+            usdResult.innerHTML = brlCurrency('en-US', 'USD', value / usdValue)
+        })
+        //.eur-brl
+        document.querySelector('.eur-brl .result').innerHTML = brlCurrency('pt-BR', 'BRL', 1 * eurValue)
+        document.querySelector('.eur-brl input').addEventListener('input', function(event) {
+            let value = event.target.value
+            let eurResult = document.querySelector('.eur-brl .result')
+            eurResult.innerHTML = brlCurrency('pt-BR', 'BRL', value * eurValue)
+        })
+        //.brl-eur
+        document.querySelector('.brl-eur .result').innerHTML = brlCurrency('de-DE', 'EUR', 1 / eurValue)
+        document.querySelector('.brl-eur input').addEventListener('input', function(event) {
+            let value = event.target.value
+            let eurResult = document.querySelector('.brl-eur .result')
+            eurResult.innerHTML = brlCurrency('de-DE', 'EUR', value / eurValue)
+        })
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  getData()
